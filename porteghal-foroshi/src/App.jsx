ce06703c1b28fa8b87cb5f5db74d6f0e5aefe3f5
AmirHosein Deldar <Amirhoseindeldar@yahoo.com>
2024-04-30 15:48:42 +0330

    whereTobuy Done

--- porteghal-foroshi/src/App.jsx
+++ porteghal-foroshi/src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
+import AppBanner from "./components/AppBanner/AppBanner";
 import Hero from "./components/Hero/Hero";
 import Services from "./components/Services/Services";
+import WhereToBuy from "./components/WhereToBuy/WhereToBuy";
 
 function App() {
   return (
     <main className="overflow-x-hidden">
       <Hero />
       <Services />
+      <WhereToBuy />
+      <AppBanner />
     </main>
   );
 }
--- porteghal-foroshi/src/components/WhereToBuy/WhereToBuy.jsx
+++ porteghal-foroshi/src/components/WhereToBuy/WhereToBuy.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import WorldMap from "../../assets/world-map.png";
 
 const WhereToBuy = () => {
-  return <div>WhereToBuy</div>;
+  return (
+    <>
+      <div className="container my-36">
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 place-items-center">
+          <div className="space-y-8">
+            <h1 className="text-4xl font-bold text-dark font-serif">
+              کجا میتونید محصولات مارو تهیه کنید؟
+            </h1>
+            <div className="flex items-center gap-4">
+              <input
+                type="text"
+                placeholder="کشور"
+                className="input-style w-full lg:w-[120px]"
+              />
+              <input type="text" placeholder="کدپستی" className="input-style" />
+            </div>
+            <button className="primary-btn">سرچ کنید</button>
+          </div>
+          <div className="col-span-2">
+            <img
+              src={WorldMap}
+              alt="map"
+              className="w-full sm:w-[500px] mx-auto"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default WhereToBuy;
